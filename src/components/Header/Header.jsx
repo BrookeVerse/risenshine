2aca3a897ff0548c52d48b02f65f21d030e8b198
BrookVerse <brookewaldorf90@gmail.com>
2022-11-10 17:00:21 +0000

    Added Scss to header and profileHeader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,17 @@ import ProfileHeader from "../ProfileHeader/ProfileHeader";
 import profile from "../../assets/images/profile.jpg";
 
 const Header = () => {
-    const date = new Date();
-    const current_date = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-    const current_time = date.getHours()+":"+ date.getMinutes();
+  const date = new Date();
+  const current_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  const current_time = date.getHours() + ":" + date.getMinutes();
   return (
-    <header>
-        <h2>Rise 'n' Shine</h2>
-        <ProfileHeader image={profile} name={"Brooke"} date={current_date} time={current_time}/>
+    <header className="header">
+      <div className="header__layout">
+        <h2 className="header__title">Rise 'n' Shine</h2>
+        <ProfileHeader image={profile} name={"Brooke"} date={current_date} time={current_time} />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,14 +1,16 @@
-import "./ProfileHeader.scss"
+import "./ProfileHeader.scss";
 
-const ProfileHeader = ({name, image, date, time}) => {
+const ProfileHeader = ({ name, image, date, time }) => {
   return (
     <div className="profileHeader">
-        <img src={image} alt="" />
+      <img src={image} alt="" className="profileHeader__image" />
+      <div>
         <h3>{name}</h3>
         <p>{date}</p>
         <p>{time}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileHeader
+export default ProfileHeader;
